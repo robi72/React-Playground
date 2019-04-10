ddd9dc35c91d4adc961f4f4b68d5d9189a69eea7
Robert Grahovac <robert.grahovac@hotmail.com>
2019-04-10 15:42:15 +0200

    error boundary

---
+++ React-Playground/src/components/errorBooundary.tsx
@@ -0,0 +1,37 @@
+import React, { CSSProperties, ErrorInfo } from 'react';
+
+
+interface Props{
+    fallbackUI: JSX.Element
+}
+
+interface State{
+    hasError: boolean
+}
+
+export default class ErrorBoundary extends React.Component<Props, State>  {
+    constructor(props: Props)  {
+        super(props);
+        this.state = { hasError: false };
+    }
+    static getDerivedStateFromError(error: Error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+    
+    componentDidCatch(error: Error, info: ErrorInfo)  {
+    // You can also log the error to an error reporting service
+    
+    console.log(error, info);
+}
+
+    render()    {
+    if (this.state.hasError)    {
+        // You can render any custom fallback UI
+
+        return this.props.fallbackUI
+    }
+    
+    return this.props.children
+    }
+}
