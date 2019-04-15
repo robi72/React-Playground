b5b2d29e0907e198f825ef9cd1433fdcc6d40665
Robert Grahovac <robert.grahovac@hotmail.com>
2019-04-15 11:18:19 +0200

    update

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,6 +3,7 @@ import Layout from './layout';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Spinner from './spinner';
 import ErrorBoundary from './errorBoundary';
+import Monkey from './monkey';
 
 /**
  * Application top level component. This is a good place for future
